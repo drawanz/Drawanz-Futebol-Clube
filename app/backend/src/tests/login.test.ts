dc28824b77c7467cfcb8e0a421dfccb0b6563b90
Leonidas <leonidas.drawanz@gmail.com>
2022-08-19 15:56:46 -0300

    feat: adicionando teste para a rota de Login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,8 +1,8 @@
-import * as sinon from 'sinon';
-const Sinon = require('sinon')
-const chai = require('chai')
+// import * as sinon from 'sinon';
+const Sinon = require('sinon');
+const chai = require('chai');
 import IDataValues from '../interfaces/IDataValues';
-const chaiHttp = require('chai-http')
+const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
@@ -17,12 +17,12 @@ const userMock: IDataValues = {
   username: 'teste',
   role: 'teste',
   email: 'teste@gmail.com',
-  password: 'teste123456',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
 }
 
 const bodyMock: IBodyReq = {
-  email: 'teste@gmail.com',
-  password: 'teste123456',
+  email: "admin@admin.com",
+  password: "secret_admin"
 }
 
 const tokenMock = {
@@ -31,22 +31,17 @@ const tokenMock = {
 
 describe('Testing Users', () => {
   describe('Testing Login', () => {
-    beforeEach(() => {
+    it(`should return status code 200, a body with a propery named token and 
+    the token should be a string`, async () => {
       Sinon.stub(User, "findOne").resolves(userMock as User);
-    })
-    
-    afterEach(() => {
-      Sinon.restore();
-    })
-
-    it('should return status code 200', async () => {
       const response = await chai.request(app).post('/login').send(bodyMock);
-      console.log(response);
       expect(response.status).to.equal(200);
+      // console.log(response);
+      expect(response.body).to.have.property('token');
+      expect(response.body.token).to.be.a('string');
+      Sinon.restore();
     })
   })
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
+
 });
