e6dc2dd792506e1c42a5dd223fc17adf4aed00fe
Leonidas <leonidas.drawanz@gmail.com>
2022-08-18 21:45:32 -0300

    feat: criando classe UsersController

---
+++ app/backend/src/controllers/UsersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import IUserService from '../interfaces/IUserService';
+
+export default class UserController {
+  private _userService: IUserService;
+
+  constructor(userService: IUserService) {
+    this._userService = userService;
+  }
+
+  public async login(req: Request, res: Response): Promise<void> {
+    const token = await this._userService.login(req.body);
+    res.status(200).send({ token });
+  }
+
+  public async loginValidate(req: Request, res: Response): Promise<void> {
+    const { authorization } = req.headers;
+    const role = await this._userService.loginValidate(authorization as string);
+    res.status(200).send({ role });
+  }
+}
