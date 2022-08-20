f0a4ecc701a014053bd50f344d87f35110824bea
Leonidas <leonidas.drawanz@gmail.com>
2022-08-20 17:00:44 -0300

    chore: criando repositories para teams e para users

---
+++ app/backend/src/repositories/TeamsRepository.ts
@@ -0,0 +1,20 @@
+import ITeam from '../interfaces/teamsInterfaces/ITeam';
+import Teams from '../database/models/teams';
+
+export default class TeamsRepository {
+  private _teamsRepository;
+
+  constructor(private teamsRepository = Teams) {
+    this._teamsRepository = teamsRepository;
+  }
+
+  public async list(): Promise<ITeam[]> {
+    const response = await this._teamsRepository.findAll();
+    return response as ITeam[];
+  }
+
+  public async listById(id: string): Promise<ITeam> {
+    const response = await this._teamsRepository.findByPk(id);
+    return response as ITeam;
+  }
+}
---
+++ app/backend/src/repositories/UserRepository.ts
@@ -0,0 +1,12 @@
+import IUserMethods from '../interfaces/userInterfaces/IUserMethods';
+import User from '../database/models/user';
+import IDataValues from '../interfaces/userInterfaces/IDataValues';
+
+export default class UserRepository implements IUserMethods {
+  constructor(private user = User) { }
+
+  public async findOne(email: string): Promise<IDataValues> {
+    const response = await this.user.findOne({ where: { email } });
+    return response as IDataValues;
+  }
+}
