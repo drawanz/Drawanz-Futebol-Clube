2a0caa467b60453d64a207dd6c5f9b154a2d19d4
Leonidas <leonidas.drawanz@gmail.com>
2022-08-22 18:42:50 -0300

    feat: adicionando update de inProgress no repositories

--- app/backend/src/repositories/MatchesRepository.ts
+++ app/backend/src/repositories/MatchesRepository.ts
@@ -1,7 +1,6 @@
 import IMatches from '../interfaces/matchesInterfaces/IMatches';
 import Matches from '../database/models/Matches';
 import IMatchesMethods from '../interfaces/matchesInterfaces/IMatchesMethods';
-import Teams from '../database/models/Teams';
 
 export default class MatchesRepository implements IMatchesMethods {
   private _matchessRepository;
@@ -10,18 +9,8 @@ export default class MatchesRepository implements IMatchesMethods {
     this._matchessRepository = matchessRepository;
   }
 
-  public async list(): Promise<IMatches[]> {
-    const response = await this._matchessRepository.findAll({
-      include: [{
-        model: Teams,
-        as: 'teamHome',
-        attributes: ['teamName'],
-      }, {
-        model: Teams,
-        as: 'teamAway',
-        attributes: ['teamName'],
-      }],
-    });
+  public async list(query: object): Promise<IMatches[]> {
+    const response = await this._matchessRepository.findAll(query);
     return response as IMatches[];
   }
 
@@ -43,5 +32,11 @@ export default class MatchesRepository implements IMatchesMethods {
       awayTeamGoals: response.getDataValue('awayTeamGoals'),
       inProgress: response.getDataValue('inProgress'),
     };
+  }
+
+  public async changeProgress(id: number): Promise<object> {
+    await this._matchessRepository
+      .update({ inProgress: false }, { where: { id } });
+    return { message: 'Finished' };
   }
 }
